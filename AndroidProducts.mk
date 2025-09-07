1784c9f7f7698dc77025a8d3cafb2d8eb0b08e9e
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:03:42 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_nairo.mk
+    $(LOCAL_DIR)/lmodroid_nairo.mk
--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/motorola/sm7250-common/common.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lmodroid
 
 PRODUCT_PACKAGES += \
     AvoidAppsInCutoutOverlay \
